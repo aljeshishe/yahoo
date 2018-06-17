9723c71ca8f9f25bb14d48648346c58793555fd9
alexey.grachev <alexey.grachev@nordigy.ru>
2018-06-17 23:16:14 +0300

    update every 5 minutes

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from yahoo import output, process, prepare, tickers
 if __name__ == '__main__':
     while True:
         output(process(prepare(tickers)))
-        time.sleep(60)
+        time.sleep(60 * 5)
