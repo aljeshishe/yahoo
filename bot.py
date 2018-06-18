eac6f0fef0b1b1cfbb9756b5ad6fe8c85f28d9a4
alexey.grachev <alexey.grachev@nordigy.ru>
2018-06-18 12:23:59 +0300

    added telegram bot

---
+++ bot.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Simple Bot to send timed Telegram messages.
+
+# This program is dedicated to the public domain under the CC0 license.
+
+This Bot uses the Updater class to handle the bot and the JobQueue to send
+timed messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Alarm Bot example, sends a message after a set time.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+from yahoo import process, prepare, tickers
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+subscribers = set()
+
+new_results = []
+notified_results = {}
+
+
+def start(bot, update):
+    subscribers.add(update.message.chat_id)
+    update.message.reply_text('Subscribing')
+    results_str = []
+    global new_results
+    for item in sorted(new_results, key=lambda result: result.period(30).percent, reverse=True):
+        periods_strs = ['{per.percent:2.1f}'.format(per=item.period(days), days=days) for days in item.periods[:-1]]
+        results_str.append('{self.ticker:5.5s}\t{periods}'.format(self=item, open=item[0], close=item[-1], periods='\t'.join(periods_strs)))
+    update.message.reply_text('\n'.join(results_str))
+
+
+def stop(bot, update):
+    subscribers.remove(update.message.chat_id)
+    update.message.reply_text('Unsubscribing')
+
+
+def update(bot, job):
+    global new_results, notified_results
+    new_results = process(prepare(tickers))
+    for item in sorted(new_results, key=lambda result: result.period(30).percent, reverse=True):
+        log.info(item.debug_info())
+
+    results_str = []
+    for new in new_results:
+        notified = notified_results.setdefault(new.ticker, new)
+        for period, new_percent, old_percent, diff in new.compare(notified):
+            if diff < -1:
+                results_str.append('{:5.5s}\t{}d\told:{}\tnew:{}'.format(notified.ticker, period, old_percent, new_percent))
+                notified_results[new.ticker] = new
+
+    if results_str:
+        for subscriber in subscribers:
+            bot.send_message(chat_id=subscriber, text='\n'.join(results_str))
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    log.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """Run bot."""
+    updater = Updater('590223940:AAEcsJfRqDYmyP5lDOvuhYXReXYb221-ozU', request_kwargs=dict(proxy_url='https://178.238.235.228:3128'))
+
+    updater.job_queue.run_repeating(update, interval=60*5, first=0)
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("stop", stop))
+    dispatcher.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(MessageHandler(Filters.text, start))
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
--- yahoo.py
+++ yahoo.py
@@ -1,8 +1,11 @@
 import logging
+import random
 from datetime import datetime, timedelta, timezone
 from functools import lru_cache
 
 import requests
+
+from tickers import tickers
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -29,11 +32,15 @@ class Candle:
         return self.datetime.strftime('%m%d-%H%M')
 
 
-class Result(list):
+class Period(list):
+    periods = [1, 7, 30, 90, 180]
+
     def __init__(self, ticker, data):
-        super(Result, self).__init__(data)
+        super(Period, self).__init__(data)
         self.ticker = ticker
         self.days = self[-1].datetime - self[0].datetime
+        self.highest = max(self, key=lambda val: val.high)
+        self.percent = (self[-1].close / self.highest.high - 1) * 100
         # self.dump()
 
     def __hash__(self):
@@ -46,20 +53,22 @@ class Result(list):
     @lru_cache()
     def period(self, days_back):
         datetime_back = self[-1].datetime - timedelta(days_back)
-        return Result(ticker=self.ticker, data=list(filter(lambda item: item.datetime > datetime_back, self)))
+        return Period(ticker=self.ticker, data=list(filter(lambda item: item.datetime > datetime_back, self)))
+
+    def compare(self, other):
+        my_results = self.results
+        other_results = other.results
+        diff = [(period, my_result, other_result, my_result - other_result) for period, my_result, other_result in zip(self.periods, my_results, other_results)]
+        # log.info(diff)
+        return diff
 
     @property
-    def percent(self):
-        return self[-1].close / self.highest.high - 1
+    def results(self):
+        return [self.period(days).percent for days in self.periods]
+        # return [random.randint(-3, 0) for days in self.periods]
 
-    @property
-    @lru_cache()
-    def highest(self):
-        return max(self, key=lambda val: val.high)
-
-    def info(self):
-        periods = [1, 7, 30, 90, 180]
-        periods_strs = ['{per.percent:.2%}:{per.highest.high:.2f}({per.highest.when})-{days}d'.format(per=self.period(days), days=days) for days in periods]
+    def debug_info(self):
+        periods_strs = ['{per.percent:2.2f}%:{per.highest.high:.2f}({per.highest.when})-{days}d'.format(per=self.period(days), days=days) for days in self.periods]
         return '{self.ticker:6.6s}\topen:{open.open:.2f}({open.when})\tclose:{close.close:.2f}({close.when})\t{periods}'.format(self=self,
                                                                                                                                 open=self[0],
                                                                                                                                 close=self[-1],
@@ -77,7 +86,7 @@ def process(tickers):
         end_ts = int(end.astimezone(timezone.utc).timestamp())
         # log.info('%s %s %s %s %s' % (ticker, start, end, start_ts, end_ts))
         resp = requests.get(r'https://query1.finance.yahoo.com/v8/finance/chart/%s' % ticker,
-                            timeout=(5, 5),
+                            timeout=(10, 10),
                             params=dict(symbol=ticker,
                                         period1=start_ts,  # in utc
                                         period2=end_ts,  # in utc
@@ -93,72 +102,31 @@ def process(tickers):
         result = data['chart']['result'][0]
         quotes = result['indicators']['quote'][0]
         data = [Candle(*values) for values in zip(result['timestamp'], quotes['open'], quotes['high'], quotes['low'], quotes['close'])]
-        results.append(Result(ticker=ticker, data=data))
+        results.append(Period(ticker=ticker, data=data))
     return results
 
 
 def output(results):
     for item in sorted(results, key=lambda result: result.period(30).percent, reverse=True):
-        log.info(item.info())
+        log.info(item.debug_info())
 
 
 def prepare(tickers):
     return filter(None, tickers.splitlines())
 
 
-tickers = """
-^IXIC
-ADBE
-ALGN
-AMZN
-CME
-EA
-ETFC
-FB
-GOOG
-IDXX
-INTC
-INTU
-LRCX
-MA
-NDAQ
-NFLX
-NOC
-NVDA
-PGR
-RTN
-SPGI
-TXN
-UNH
-V
-VLO
-
-AAPL
-ALL
-AMAT
-ATVI
-BA
-BABA
-BBY
-CNC
-CTAS
-EL
-FLIR
-ISRG
-KLAC
-MAS
-MPC
-MU
-NTAP
-SYY
-TWTR
-WM
-XYL
-"""
-
 if __name__ == '__main__':
     output(process(prepare(tickers)))
+
     # output(process(prepare('lrcx')))
+
+    # lrcx = process(prepare('lrcx'))[0]
+    # adbe = process(prepare('adbe'))[0]
+    # print(lrcx.info())
+    # print(adbe.info())
+    # print(lrcx.compare(adbe))
+
+
 
 # response example
 # {
